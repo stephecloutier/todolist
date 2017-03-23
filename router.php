62ddcacc3f42f3d3f7a86977cab9d2c73bbd316c
stephecloutier <stephecloutier@gmail.com>
2017-03-23 10:02:48 +0100

    Completed router file

--- router.php
+++ router.php
@@ -9,3 +9,13 @@
 session_start();
 
 require 'configs/routes.php';
+$default_route = $routes['default'];
+$route_parts = explode('/', $default_route);
+$method = $_SERVER['REQUEST_METHOD'];
+$r = $_REQUEST['r']??$route_parts[1]; // récupérer la Ressource
+$a = $_REQUEST['a']??$route_parts[2];// récupérer l'Action
+$id = $_REQUEST['id']??$route_parts[3]; // récupérer l'ID (Si besoin ?)
+
+// $controllerFile = $r . 'Controller.php'; lorsque j'aurai séparé mes contrôlleurs
+require 'controllers/controller.php';
+$data = call_user_func($a); // ?
