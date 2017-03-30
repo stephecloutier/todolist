1da6bf1b933540528734e8da242b1f666fd507ef
stephecloutier <stephecloutier@gmail.com>
2017-03-30 09:00:47 +0200

    Updated logout with proper session_destroy

--- controllers/authController.php
+++ controllers/authController.php
@@ -12,7 +12,16 @@ function getLogin ()
 
 function getLogout ()
 {
-    session_destroy();
+    if(ini_get("session.use_cookies")){
+        $params = session_get_cookie_params();
+
+        setcookie(session_name(), '', time(1),
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+        session_destroy();
+    }
+
     header('Location: http://homestead.app/pwcs/todolist/index.php');
 }
 
